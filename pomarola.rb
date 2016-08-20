4065e296e6196c88048ef4cebf64f1196f128692
Juan F. Giménez Silva <juanfgs@gmail.com>
2016-08-20 17:26:24 -0300

    added popover menu and tray icons

--- pomarola.rb
+++ pomarola.rb
@@ -8,6 +8,7 @@
 
 require "gtk3"
 require "glib2"
+require "json"
 require "notify"
 require "pp"
 require_relative "lib/pomodoro"
@@ -15,15 +16,25 @@ require_relative "lib/pomodoro"
 
 class Pomarola
   attr_accessor :current_pomodoro, :pomodoros, :loop
+  
   def initialize
     @stopped_pomodoro = true
     @pomodoro_count = 0
     @pomodoro_length = 1500
     @break_length = 300
     @long_break_multiplier = 4
+
+
+    @file_filter = Gtk::FileFilter.new
+    @file_filter.name = "Pomarola Worklogs (*.pom)"
+    @file_filter.add_pattern("*.pom")
+
     
     @ui = Gtk::Builder.new
     @ui.add_from_file "ui/pomarola.ui"
+
+    @status_icon = Gtk::StatusIcon.new 
+    @status_icon.file =  "ui/icon.png" 
     
     #connect all handlers except the start_pomodoro 
     @ui.connect_signals do |handler|
@@ -43,9 +54,14 @@ class Pomarola
     @pomodoro_renderer = Gtk::CellRendererText.new
     @past_pomodoros_view.model = @past_pomodoros_list
 
+
+    
+    generate_popover
+
+    
     @cols = [
       Gtk::TreeViewColumn.new("Name", @pomodoro_renderer_label, :text => 0),
-      Gtk::TreeViewColumn.new("Time Rested", @pomodoro_renderer, :text => 1),
+      Gtk::TreeViewColumn.new("Time Started", @pomodoro_renderer, :text => 1),
     ]
     
     @cols.each do |col|
@@ -63,17 +79,68 @@ class Pomarola
     @main_window.signal_connect "destroy" do
       Gtk.main_quit
     end
+    
     @main_window.show_all
   end
 
+  def generate_popover()
+    @popover = @ui.get_object "popover1"
+    @popover_ui = Gtk::Builder.new(:file => "./ui/menu.ui")
+    @popover_contents = @popover_ui["popover"]
+    
+    @popover_ui.connect_signals do |handler|
+        method(handler)
+    end
 
+   
+    @popover.add @popover_contents
+  end
+  
   # This handler is triggered by pressing the start button
   # it should start the countdown for the pomodoro to expire
   #
   # ==== Attributes
   # * +widget+ - GtkButton : the originating widget
   #
+
+  def quit()
+    Gtk.main_quit
+  end
+
+  def save_file()
+    dialog = Gtk::FileChooserDialog.new({
+                                          :title => "Save File",
+                                          :parent => @main_window,
+                                          :action => Gtk::FileChooserAction::SAVE,
+                                          :buttons => [  [Gtk::Stock::CANCEL, Gtk::ResponseType::CANCEL],[Gtk::Stock::SAVE, Gtk::ResponseType::ACCEPT] ]
+                                        })
+    
+    dialog.add_filter @file_filter
+    time = Time.new
+    dialog.current_name  = "#{time.year}-#{time.month}-#{time.day} Work Log.pom"
+    
+    if dialog.run == Gtk::ResponseType::ACCEPT
+      puts "filename = #{dialog.filename}"
+    end
+    dialog.destroy
+  end
   
+  def open_file()
+    dialog = Gtk::FileChooserDialog.new({
+                                          :title => "Save File",
+                                          :parent => @main_window,
+                                          :action => Gtk::FileChooserAction::OPEN,
+                                          :buttons => [  [Gtk::Stock::CANCEL, Gtk::ResponseType::CANCEL],[Gtk::Stock::OPEN, Gtk::ResponseType::ACCEPT] ]
+                                        })
+     
+    dialog.add_filter @file_filter
+    
+    if dialog.run == Gtk::ResponseType::ACCEPT
+      puts "filename = #{dialog.filename}"
+    end
+    dialog.destroy
+  end
+ 
   def start_pomodoro(widget)
     if @stopped_pomodoro #If the pomodoro is returning from stopped, we create a new pomodoro
       @stopped_pomodoro = false
@@ -94,13 +161,15 @@ class Pomarola
         if remaining.to_i <= @current_pomodoro.break_duration  #we notify the user this cycle is about to end
           
           @timer.set_text @current_pomodoro.time_remaining.strftime("(Break) %M:%S")
-          
+          @status_icon.set_tooltip_text @current_pomodoro.time_remaining.strftime("(Break) %M:%S")
           if !@break_notify
             Notify.notify "Time to rest", "This cycle is over, time for some rest"
             @break_notify = true
           end
         else
           @timer.set_text @current_pomodoro.work_time_remaining.strftime("%M:%S")
+          
+          @status_icon.set_tooltip_text @current_pomodoro.work_time_remaining.strftime("%M:%S")
         end
         
         if remaining.to_i <= 2 && !@end_notify  #we notify the user this cycle is about to end
@@ -109,7 +178,6 @@ class Pomarola
         end
         
         true # continue in the loop
-        
       elsif !@stopped_pomodoro &&  (remaining.to_i <= 0) 
         update_log(@current_pomodoro)
         if @pomodoro_count == 3 #check whether is time for the long break 
@@ -126,11 +194,12 @@ class Pomarola
       end
     end
   end
+  
   #
   # updates the log list
   #
   def update_log(pomodoro)
-    diff = pomodoro.start - pomodoro.break_duration
+    diff = pomodoro.start
     iter = @past_pomodoros_list.append()
     iter[0] = '(Insert Label here)'
     iter[1] = diff.strftime("%M:%S")
@@ -149,14 +218,15 @@ class Pomarola
       
     case status
     when :start
+      @status_icon.file = "ui/icon-pause.png"
       @start_button.label = Gtk::Stock::MEDIA_PLAY
       @start_button.set_image Gtk::Image.new( :stock =>  Gtk::Stock::MEDIA_PLAY)
- 
       @start_button.signal_handler_disconnect @start_pomodoro_id
       @start_pomodoro_id = @start_button.signal_connect "clicked" do |button|
         start_pomodoro(button)
       end
     when :pause
+      @status_icon.file = "ui/icon-play.png"
       @start_button.label = Gtk::Stock::MEDIA_PAUSE
       @start_button.set_image Gtk::Image.new( :stock =>  Gtk::Stock::MEDIA_PAUSE)
       @start_button.signal_handler_disconnect @start_pomodoro_id
